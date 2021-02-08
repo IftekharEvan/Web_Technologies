be9d3fa7722c564c53d2f7822ba180df0064b472
IftekharEvan <78031728+IftekharEvan@users.noreply.github.com>
2021-02-08 14:10:14 +0600

    Add files via upload

---
+++ Lab_Task_1_Registration.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<title> Registration </title>
+
+</head>
+
+<body>
+<form>
+<table>
+<tr>
+<td>Name :</td> 
+<td><input type="text" id = "a" name = "e" ></td>
+</tr>
+<tr>
+<td> Email : </td>
+<td> <input type="text" id = "d" name = "h" ></td>
+</tr>
+<td> User Name : </td>
+<td> <input type="text" id = "b" name = "f" ></td>
+</tr>
+<tr>
+<td> Password : </td>
+<td> <input type = "password" id = "a" name = "e"> </td>
+</tr>
+<tr>
+<td> Confirm Password : </td>
+<td> <input type = "password" id = "a" name = "e"> </td>
+</tr>
+<tr>
+<td> Gender : </td>
+<td> <input type = "radio" id = "male" name = "gender" value = "male"> Male </td>
+<td> <input type = "radio" id = "female" name = "gender" value = "female"> Female </td>
+<td> <input type = "radio" id = "others" name = "gender" value = "others"> Others </td>
+</tr>
+<tr>
+<td> Date Of Birth : </td>
+<td> <input type = "text" id = "a" name = "e"> </td>
+</tr>
+<tr>
+<td><input type = "submit" value = "SUBMIT"></td>
+<td><input type = "reset" value = "RESET"></td>
+</tr>
+
+</table>
+
+</form>
+
+</body>
+
+</html>
